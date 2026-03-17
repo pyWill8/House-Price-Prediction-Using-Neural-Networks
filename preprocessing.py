168dce595abb3b121936b7398a952871a704a734
nrwq6940 <nrwq6940@leeds.ac.uk>
2026-03-17 13:29:18 +0000

    Add files via upload
    added feature construction to be called upon

---
+++ preprocessing.py
@@ -0,0 +1,64 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.datasets import fetch_california_housing
+
+def df_basic_cleaning_and_split(data = fetch_california_housing(as_frame=True).frame,
+    target_col="MedHouseVal",
+    test_size=0.3,
+    random_state=42):
+    
+    df = data.copy() 
+
+    # Basic cleaning
+    df = df.drop_duplicates()
+    df = df.dropna()
+
+    X = df.drop(columns=[target_col])
+    y = df[target_col]
+
+    # Train test split first (prevents leakage)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, 
+                                                        random_state=random_state)
+    
+    return X_train, X_test, y_train, y_test
+
+def array_standardise_data(X_train, X_test, y_train, y_test):
+    # Turning the data into a numpy array 
+    X_train = np.array(X_train)
+    X_test = np.array(X_test)
+    y_train = np.array(y_train).reshape(-1, 1)
+    y_test = np.array(y_test).reshape(-1, 1)
+
+    # Standardisation
+    standardisation = StandardScaler()
+    standardisation.fit(X_train)
+    X_train = standardisation.transform(X_train)
+
+    # Parameters from the X_train used on X_test to prevent leakage.
+    X_test = standardisation.transform(X_test)
+
+    return X_train, X_test, y_train, y_test
+
+def df_feature_constructions(X_train, X_test):
+
+    X_train = X_train.copy()
+    X_test = X_test.copy()
+
+    # Ratio Features
+    X_train["rooms_per_household"] = X_train["AveRooms"] / X_train["AveOccup"]
+    X_test["rooms_per_household"] = X_test["AveRooms"] / X_test["AveOccup"]
+
+    X_train["bedroom_ratio"] = X_train["AveBedrms"] / X_train["AveRooms"]
+    X_test["bedroom_ratio"] = X_test["AveBedrms"] / X_test["AveRooms"]
+
+    # Density feature
+    X_train["population_density"] = X_train["Population"] / X_train["AveOccup"]
+    X_test["population_density"] = X_test["Population"] / X_test["AveOccup"]
+
+    # Interaction feature
+    X_train["income_rooms"] = X_train["MedInc"] * X_train["AveRooms"]
+    X_test["income_rooms"] = X_test["MedInc"] * X_test["AveRooms"]
+
+    return X_train, X_test
